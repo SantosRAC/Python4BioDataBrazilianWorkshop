2ef6d7708c6861af15e19f1e0565f1f92c868f9c
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-07-19 10:51:45 -0300

    Reading table (fields separated by tab) with the read_table function of the pandas library; checking files in current directory

--- Problem1/data_analysis_example1.py
+++ Problem1/data_analysis_example1.py
@@ -1,2 +1,7 @@
-import pandas
+import pandas as pd # Importing python library called 'pandas'
+import os # Used to check files in directory
+
+os.listdir() # Lists files in the current dir (Windows 10)
+
+pd.read_table('nmeth.1226-S3.txt') # Convert text data into a DataFrame
 
