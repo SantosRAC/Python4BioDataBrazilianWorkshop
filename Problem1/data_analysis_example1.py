675623d60317ab280243e7b5a0f90829e614d834
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-07-19 13:43:12 -0300

    Assigning result of the read_table function to a Python object (gene expression data in supp mat S3 of the main reference of problem 1)

--- Problem1/data_analysis_example1.py
+++ Problem1/data_analysis_example1.py
@@ -1,7 +1,17 @@
+# Several simple tests, such as
+## importing libraries
+## reading table
+## reading content of directories
+##
+##
+
 import pandas as pd # Importing python library called 'pandas'
 import os # Used to check files in directory
+import Bio # Importing biopython
 
 os.listdir() # Lists files in the current dir (Windows 10)
 
 pd.read_table('nmeth.1226-S3.txt') # Convert text data into a DataFrame
+
+tabelaExpressao = pd.read_table('nmeth.1226-S3.txt') # Importing the table with gene expression
 
