75cbd0b8107e242140210dbb23504162663d68b6
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-08-08 15:12:53 -0300

    Exploring pandas DataFrame (gene expression data)

--- Problem1/data_analysis_example1.py
+++ Problem1/data_analysis_example1.py
@@ -9,9 +9,28 @@ import pandas as pd # Importing python library called 'pandas'
 import os # Used to check files in directory
 import Bio # Importing biopython
 
-os.listdir() # Lists files in the current dir (Windows 10)
+print(os.listdir()) # Lists files in the current dir (it works either on Windows 10 or on Ubuntu 14.04)
 
-pd.read_table('nmeth.1226-S3.txt') # Convert text data into a DataFrame
+print(pd.read_table('nmeth.1226-S3.txt')) # Reading gene expression data with "read_table" function of pandas
 
-tabelaExpressao = pd.read_table('nmeth.1226-S3.txt') # Importing the table with gene expression
+tabelaExpressao = pd.read_table('nmeth.1226-S3.txt') # Importing the table with gene expression data
 tabelaAnotacaoCamundongo = pd.read_table('gene_result.txt') # Importing the table with annotation (from NCBI)
+
+print(tabelaExpressao) # Printing the pandas DataFrame with gene expression data
+                       ## Python will limit number of lines shown
+
+print(tabelaExpressao.columns) # Printing the columns of the DataFrame
+
+print(tabelaExpressao['gene']) # Retrieving information of a column by dict-like notation
+
+print(tabelaExpressao.gene) # Retrieving information of a column by attribute
+
+print(tabelaExpressao.ix[1]) # Retrieving information from column 1
+
+print(tabelaExpressao.ix[[1, 2]]) # Retrieving information from column 1 and 2
+                                 ## It is done by giving method ix a list with the row indexes
+
+print(tabelaExpressao.ix[[40, 401]]) # Retrieving information from column 40 and 401
+
+
+
